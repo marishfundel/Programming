d2bf01a39255c1fbe7cf06daf5f52ec8ae251d09
Marina <marishfundel@yandex.ru>
2024-04-02 23:47:29 -0400

    class Max number completed

--- src/m32_arrays_part2/MaxNumberFromArray.java
+++ src/m32_arrays_part2/MaxNumberFromArray.java
@@ -1,10 +1,24 @@
 package m32_arrays_part2;
 
 public class MaxNumberFromArray {
-
-    public static int findMaxNumber (int[] numbers){
-
+    public static void main(String[] args) {
+        int[] numbers = {6, 8, 3, 5, 1, 10};
+        findMaxNumber(numbers);
     }
 
+    public static int findMaxNumber(int[] numbers) {
+        if (numbers == null || numbers.length == 0) {
+            System.out.println("Array is empty");
+            return 0;
+        }
 
+        int max = numbers[0];
+        for (int number : numbers) {
+            if (number > max) {
+                max = number;
+            }
+        }
+        System.out.println(max);
+        return max;
+    }
 }
