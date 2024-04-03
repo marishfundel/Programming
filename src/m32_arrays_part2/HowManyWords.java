08a27c28dda44840d53096544a9860d4a9bf4e57
Marina <marishfundel@yandex.ru>
2024-04-03 17:59:52 -0400

    Class HowManyWords completed

--- src/m32_arrays_part2/HowManyWords.java
+++ src/m32_arrays_part2/HowManyWords.java
@@ -2,7 +2,16 @@ package m32_arrays_part2;
 
 public class HowManyWords {
     public static void main(String[] args) {
-
+        String str = "In computer programming white space refers to any character or sequence of characters " +
+                "that represents empty space or have no visible representation when rendered.";
+        String[] words = str.split(" ");
+        char[] arr = str.toCharArray();
+        System.out.println(arr.length);
+        int amountOfWords = 0;
+        for (int i = 1; i <= words.length; i++) {
+            amountOfWords += 1;
+        }
+        System.out.println(amountOfWords);
 
     }
 }
