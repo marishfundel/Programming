dc35ba82869730838a1bb557284b3b6cf3d384cd
Marina <marishfundel@yandex.ru>
2024-04-02 21:04:37 -0400

    learned the shortcut of the for loop

--- src/m31_arrays_part1/ForEachLoop.java
+++ src/m31_arrays_part1/ForEachLoop.java
@@ -3,12 +3,17 @@ package m31_arrays_part1;
 public class ForEachLoop {
     public static void main(String[] args) {
 
-        int [] numbers = {15, 30, 45, 60, 75, 90};
-        for (int each : numbers){
-            System.out.println(each);
+      //  int [] numbers = {15, 30, 45, 60, 75, 90};
+      //  for (int each : numbers){
+        //    System.out.println(each);
+
+            String[] names = {"Java", "Python", "C#", "Ruby"};
+            for (String name : names) {
+                System.out.println(name);
+            }
 
         }
 
     }
 
-}
+
