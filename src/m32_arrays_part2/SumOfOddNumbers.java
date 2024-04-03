b7ad91bb4e52813b69bebf7cd4da1bc3831e35c4
Marina <marishfundel@yandex.ru>
2024-04-02 23:21:30 -0400

    The method is working correct. The class complited.

--- src/m32_arrays_part2/SumOfOddNumbers.java
+++ src/m32_arrays_part2/SumOfOddNumbers.java
@@ -1,4 +1,21 @@
 package m32_arrays_part2;
 
 public class SumOfOddNumbers {
+    public static void main(String[] args) {
+        int[] numbers = {3, 3, 2, 3, 2, 3};
+        sumOfOddNumbers(numbers);
+    }
+
+    public static int sumOfOddNumbers(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            if (number % 2 != 0) {
+                sum += number;
+            }
+        }
+        System.out.println(sum);
+        return sum;
+
+    }
+
 }
