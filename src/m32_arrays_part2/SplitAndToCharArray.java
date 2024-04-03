801ade058e6587c9d9daccf59d6cd8c3b300fa04
Marina <marishfundel@yandex.ru>
2024-04-03 16:35:12 -0400

    Class SplitAndToCharArray completed

--- src/m32_arrays_part2/SplitAndToCharArray.java
+++ src/m32_arrays_part2/SplitAndToCharArray.java
@@ -1,4 +1,18 @@
 package m32_arrays_part2;
 
+import java.util.Arrays;
+
 public class SplitAndToCharArray {
+    public static void main(String[] args) {
+        String str = "Apple, Banana, Orange, Cherry, Strawberry";
+        String[] fruits = str.split(",");
+        System.out.println(Arrays.toString(fruits));
+        System.out.println("--------------------------");
+        System.out.println();
+
+        String str2 = "Hello";
+       char[] word = str2.toCharArray();
+        System.out.println(Arrays.toString(word));
+    }
+
 }
