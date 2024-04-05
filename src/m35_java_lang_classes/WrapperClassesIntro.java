1de64621497d03c200cacddfc03f7ec6c600f21c
Marina <marishfundel@yandex.ru>
2024-04-05 02:24:00 -0400

    Class Wrapper class completed

---
+++ src/m35_java_lang_classes/WrapperClassesIntro.java
@@ -0,0 +1,13 @@
+package m35_java_lang_classes;
+
+public class WrapperClassesIntro {
+    public static void main(String[] args) {
+        Integer num1 = Integer.valueOf(1234);
+        System.out.println(num1 +1);
+        Integer num2 = Integer.valueOf("25");
+        System.out.println(num2 +1);
+
+       Double num3 =  Double.valueOf(2.5);
+        System.out.println(num3);
+    }
+}
