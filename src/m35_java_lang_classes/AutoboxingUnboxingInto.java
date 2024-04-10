05a402b74475a09471f4d0363890b374aa1de56b
Marina <marishfundel@yandex.ru>
2024-04-10 14:01:21 -0400

    Class AutoboxingUnboxingInto created

--- src/m35_java_lang_classes/AutoboxingUnboxingInto.java
+++ src/m35_java_lang_classes/AutoboxingUnboxingInto.java
@@ -1,4 +1,17 @@
 package m35_java_lang_classes;
 
 public class AutoboxingUnboxingInto {
+    public static void main(String[] args) {
+        int a = 100;
+       Integer num1 = a;
+
+       long b = 200;
+       Long num2 = b;
+
+       Integer x = 100;
+       int y = x;
+
+       Double m = 20.5;
+       double n = m;
+    }
 }
